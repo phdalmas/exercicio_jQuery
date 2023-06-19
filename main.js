5bab4e1e962d0f0d6911a76403d7b4aa51e33f3b
Pedro Dalmas <phdalforno@gmail.com>
2023-06-19 16:34:22 -0300

    first commit

--- main.js
+++ main.js
@@ -1,27 +1,14 @@
 $(document).ready(function() {
-    $('header button').click(function() {
-        $('form').slideDown();
-    });
-
-    $('#botao-cancelar').click(function(){
-        $('form').slideUp();
-    })
-
     $('form').on('submit', function(e) {
         e.preventDefault(); 
-        const enderecoDaNovaImagem = $('#endereço-imagem-nova').val();
-        const novoItem = $('<li style="display: none"></li>');
-        $(`<img src="${enderecoDaNovaImagem}"/>`).appendTo(novoItem);
-        $(`
-        
-            <div class="overlay-imagem-link">
-                <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
-                    Ver imagem em tamanho real
-                </a>
-            </div>
-            `).appendTo(novoItem);
-            $(novoItem).appendTo('ul');
-            $(novoItem).fadeIn(1000);
-            $('#endereço-imagem-nova').val('')
+
+        const tarefaNova = $('#tarefa-nova').val();
+        const novoItem = $('<li></li>');
+        $(`<li>${tarefaNova}<li/>`).appendTo(novoItem);
+        $(novoItem).appendTo('ol');
+        $("li").click(function() {
+            $('li').addClass("check");
+        });
+        $('#tarefa-nova').val("");
     })
 })
